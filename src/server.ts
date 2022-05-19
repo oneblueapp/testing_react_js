c084456fd203d4375e5244cab3c023008fc9e053
Deividy Ferreira <fnsdeividy@gmail.com>
2022-05-19 18:03:50 -0300

    add cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express, { json } from 'express'
+import cors from 'cors'
 import { routes } from './Routes/routes'
 
 const app = express()
 const PORT = 4000
-app.use(json())
+
+app.use(cors({
+  origin: '*'
+}))
+
+app.use(express.json())
 app.use(routes)
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`))
